a2d6f5f99fe703dd5f12bd6d289782548dd03a8a
PROxZIMA <pratikbpingale9075@gmail.com>
2022-04-30 19:38:36 +0530

    Fix: Resume download link

--- js/script.js
+++ js/script.js
@@ -1,20 +1,24 @@
-const LOCAL_DOMAINS = ["localhost", "127.0.0.1", ""];
+const LOCAL_DOMAINS = ["127.0.0.1", ""];
 const resumeBtn = document.getElementById('resume-btn');
 const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
 
-resumeBtn.addEventListener("click", async () => {
+resumeBtn.addEventListener("click", async (e) => {
+  let isLocalServer = false;
+  if (LOCAL_DOMAINS.includes(location.hostname)) {
+    isLocalServer = true;
+    e.preventDefault();
+  }
+
   resumeBtn.classList.add("load");
 
   await sleep(1000);
   resumeBtn.classList.add("done");
 
   await sleep(1000);
-  if (LOCAL_DOMAINS.includes(location.hostname)) {
+  if (isLocalServer) {
     document.title = "Pratik Pingale's Resume";
     window.print();
     document.title = "Resume - Pratik Pingale";
-  } else {
-    window.location.assign("./extras/Pratik Pingale's Resume.pdf");
   }
 
   await sleep(500);
